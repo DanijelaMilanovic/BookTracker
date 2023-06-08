0af9e7c66ca81977f5a412371b93822bc1852046
DanijelaMilanovic <78234982+DanijelaMilanovic@users.noreply.github.com>
2023-06-08 20:19:27 +0200

    react test fail

--- client-app/src/Tests/MyComponentFailing.test.js
+++ client-app/src/Tests/MyComponentFailing.test.js
@@ -5,6 +5,6 @@ import MyComponent from '../Component/MyComponent';
 test('renders component with correct name', () => {
   const name = 'John';
   const { getByText } = render(<MyComponent name={name} />);
-  const componentElement = getByText(`Hello, ${name}!`);
+  const componentElement = getByText(`Hellp, ${name}!`);
   expect(componentElement).toBeInTheDocument(); 
 });
